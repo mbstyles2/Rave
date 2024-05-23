5a3ccffc78961931d0dd21911bf6da1976868f8c
Mitchel Akhiotu (MB Styles) <73815025+mbstyles2@users.noreply.github.com>
2024-05-23 10:21:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// var open = false;
-// function handleMenu() {
-/** open the menu **/
-// if (!open) {
- //  document.getElementById("navigation").classList.add("opened");
-//    open = true;
-   
-    
-  } 
-/** else close the menu **/  
-// else {
-    //  document.getElementById("navigation").classList.remove("opened");
-//    open = false;
-//  }
-// }
 function toggleNav() {
  var navigation = document.getElementById("navigation");
  navigation.classList.toggle("active");
